8f532066a7eb8dc1e56358b0abdc0125958725ca
Viorel <viorel.binciu@codaintelligence.com>
2024-01-14 19:53:14 +0200

    (task:frontend) Imported Homepage into main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.scss";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Homepage from "./pages/Homepage/index.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<App />}>
+          <Route path="/" element={<Homepage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
