ebab2197c9c7dd9eebcb49f62c03e7506d93a965
Viorel <viorel.binciu@codaintelligence.com>
2024-01-14 19:51:46 +0200

    (task:frontend) Added Hero component

---
+++ frontend/src/pages/Homepage/components/Hero/index.tsx
@@ -0,0 +1,37 @@
+import { motion } from "framer-motion";
+import AnimatedTextCharacter from "../../../../components/AnimatedText";
+import BtnPrimary from "../../../../components/ButtonPrimary";
+import styles from "./index.module.scss";
+
+export default function Hero() {
+  return (
+    <div className={styles.outerWrapper}>
+      <div className={styles.wrapper}>
+        <AnimatedTextCharacter
+          text={"Welcome to CodeHabits!"}
+          paragraph={false}
+        />
+        <AnimatedTextCharacter
+          text={
+            "Learning Without Barriers, Free and Open to All. Enroll Today!"
+          }
+          paragraph
+        />
+        <motion.div
+          className={styles.buttonWrapper}
+          transition={{ ease: "easeOut", duration: 1 }}
+          initial={{ opacity: 0, y: 100 }}
+          animate={{ opacity: 1, y: 0 }}
+          whileHover={{
+            scale: 1.1,
+            transition: { duration: 1 },
+          }}
+        >
+          <a href={"/quizzes"}>
+            <BtnPrimary text="Start learning for free" />
+          </a>
+        </motion.div>
+      </div>
+    </div>
+  );
+}
