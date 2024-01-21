c9fadc8cc0e693088628d2623825126db7df04c4
Viorel <viorel.binciu@codaintelligence.com>
2024-01-21 19:04:12 +0200

    (task:frontend) Replaced index.scss with index.css

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.scss";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage/index.tsx";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
